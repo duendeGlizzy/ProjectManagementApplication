ff2389a0fe6e70964fa55665beb9414379a7315f
Steven Pacheco <151979449+duendeGlizzy@users.noreply.github.com>
2026-06-11 14:34:29 -0400

    implemented cors config

---
+++ src/main/java/com/JobTracker/demo/Config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.JobTracker.demo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig{
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+
+            }
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                configurer.addPathPrefix("/api", c -> c.isAnnotationPresent(RestController.class));
+            }
+        };
+
+
+    }
+
+
+}
