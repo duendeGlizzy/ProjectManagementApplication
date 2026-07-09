308bf0b32cb6dba3a4669741e157f523c80c6fc0
Steven Pacheco <151979449+duendeGlizzy@users.noreply.github.com>
2026-07-09 14:29:16 -0400

    changed login screen ui

--- FrontEnd/job-tracker-app/src/app/features/security/components/login/login.ts
+++ FrontEnd/job-tracker-app/src/app/features/security/components/login/login.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { AuthService} from '../../service/auth-service';
 import {Router, RouterLink} from '@angular/router';
 import {FormsModule } from '@angular/forms';
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
+import {MatIcon} from '@angular/material/icon';
+import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, MatCard, MatCardHeader, MatIcon, MatCardTitle, MatCardContent, MatFormField, MatButton, CommonModule, MatInput, MatError, MatLabel],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
