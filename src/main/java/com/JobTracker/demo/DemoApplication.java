81a42d63e92e47417b8f8ad191c9e5a6fe049cde
Steven Pacheco <151979449+duendeGlizzy@users.noreply.github.com>
2026-07-11 00:25:17 -0400

    back and frontend environment configurations

--- src/main/java/com/JobTracker/demo/DemoApplication.java
+++ src/main/java/com/JobTracker/demo/DemoApplication.java
@@ -1,14 +1,43 @@
 package com.JobTracker.demo;
 
+import com.JobTracker.demo.Entity.Admin;
+import com.JobTracker.demo.Repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Value("${ADMIN_PASS:password!}")
+	private String adminPassword;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
 	}
 
+	@Bean
+	CommandLineRunner initAdminAccount(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+
+			if (adminRepository.count() == 0) {
+				Admin admin = new Admin();
+				admin.setEmail("admin@jobtracker.com");
+
+				admin.setPassword(passwordEncoder.encode(adminPassword));
+
+				adminRepository.save(admin);
+			}
+
+
+		};
+	}
+
+
 }
+
+
+
